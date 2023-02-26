7c468e9332a860d5b51f5e8cab97f813e4643099
Asser <66274223+AsserElfeki@users.noreply.github.com>
2023-02-26 14:56:25 +0100

    footer layout

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-    <ul>
+    <ul class="hz">
       <li>
         <a class="flex" href="https://github.com/AsserElfeki/nuxtfood#readme">
           <img class="img" src="../assets/github.png" alt="github icon" />
@@ -11,7 +11,7 @@
       <li>
         <a class="flex" href="https://www.linkedin.com/in/asserelfeki/">
           <img class="img" src="../assets/linkedin.png" alt="LinkedIn icon" />
-          <p>GithubRepo</p>
+          <p>LinkedIn</p>
         </a>
       </li>
     </ul>
@@ -23,6 +23,15 @@ export default {};
 </script>
 
 <style scoped>
+.hz{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+
 .flex {
   display: flex;
   justify-content: center;
